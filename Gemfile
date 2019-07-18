f5e08e1df22081bbe62ac0aa4e2e2de8a2240c42
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-18 20:07:51 +0900

    sss
    aa

--- Gemfile
+++ Gemfile
@@ -57,3 +57,6 @@ gem 'font-awesome-rails'
 
 gem 'devise'
 
+gem 'carrierwave'
+gem 'mini_magick'
+
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
   def index
   end
 
@@ -14,6 +15,9 @@ class GroupsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
   end
   
   def update
