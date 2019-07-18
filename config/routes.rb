0c6f9eed34cae2cd64116770bc49d762fe881e03
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-18 16:49:27 +0900

    group
    aa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resources :messages, only: [:index, :create]
   resources :users, only: [:edit, :update]
 end
