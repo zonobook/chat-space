fd0f8ca02b10b50191774cd9039b67d85ca2f972
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-21 20:11:17 +0900

    7/21

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+    var img = ""
+    if (message.image !== null) {
+      img = `<img src="${message.image}">`}
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content" if message.content.present?>
+                    ${message.content}
+                    </p>
+                    ${img}
+                  </div>
+                </div>`
+    return html;
+  }
+  $('form').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $('.form__submit').removeAttr('data-disable-with');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data)
+      $('.messages').append(html)
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      $('.form__message').val('')
+      $('.hidden').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+})
