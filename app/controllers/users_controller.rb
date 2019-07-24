8f3f925713e1fdee047c2c65e7cadf02decf5de5
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-24 15:12:27 +0900

    user_jbuilder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+  end
 
   def edit
   end
