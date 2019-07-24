7e30c28065e2b7a70eff3e09dc6c0e043bc1748d
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-24 12:52:10 +0900

    delete_null

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    var img = message.image !== null ? `<img src="${message.image}">` : "";
+    var img = message.image ? `<img src="${message.image}">` : "";
     var html = `<div class="message">
                   <div class="upper-message">
                     <div class="upper-message__user-name">
