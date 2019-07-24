573a21f7311216d1593336ab15cab2c31263312f
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-24 18:07:25 +0900

    ajax

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,5 +1,13 @@
 $(function(){
   $("#user-search-field").on("keyup", function(){
-    console.log(this)
+    var input = $("#user-search-field").val();
+    console.log(input)
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html
       format.json
+    end
   end
 
   def edit
