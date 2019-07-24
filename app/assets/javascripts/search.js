842cca027f95f422563364da6184f72523e7d91d
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-24 21:25:25 +0900

    empty_pattern

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -9,35 +9,37 @@ function appendUser(user){
   search_list.append(html);
 }
 function appendErrMsgToHTML(msg){
-  var html = `<li>
-                <div class='chat-group-user clearfix'>${ msg }</div>
-              </li>`
+  var html = `<div class='chat-group-user clearfix'>${ msg }</div>`
   search_list.append(html);
 }
 
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
+    if (input.length !== 0){
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
 
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-
-    .done(function(users){
+      .done(function(users){
+        $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else{
+          appendErrMsgToHTML("一致するユーザーはいません");
+        }
+      })
+      .fail(function(){
+        alert('ユーザー検索に失敗しました');
+      })
+    }
+    else{
       $("#user-search-result").empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      }
-      else{
-        appendErrMsgToHTML("一致するユーザーはいません");
-      }
-    })
-    .fail(function(){
-      alert('ユーザー検索に失敗しました');
-    })
+    }
   });
 });
