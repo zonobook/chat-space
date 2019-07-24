429aae1568be60346f27652502ce5688bcd1d61a
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-24 20:48:59 +0900

    append

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,13 +1,43 @@
 $(function(){
+
+var search_list = $("#user-search-result");
+function appendUser(user){
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+              </div>`
+  search_list.append(html);
+}
+function appendErrMsgToHTML(msg){
+  var html = `<li>
+                <div class='chat-group-user clearfix'>${ msg }</div>
+              </li>`
+  search_list.append(html);
+}
+
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
-    console.log(input)
 
     $.ajax({
       type: 'GET',
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
+    })
+
+    .done(function(users){
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else{
+        appendErrMsgToHTML("一致するユーザーはいません");
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
     })
   });
 });
