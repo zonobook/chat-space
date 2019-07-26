58ed3b90c72840c5c735460cd25bee60fe240a53
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-26 23:25:54 +0900

    b

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,4 @@ class User < ApplicationRecord
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
-  def self.search(search)
-    @users = User.where.not(id:current_user.id)
-  end
 end
