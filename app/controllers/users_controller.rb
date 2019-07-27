697c372131ba39091a262a058d48ef99f3903bc6
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-27 14:24:16 +0900

    tired

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%", current_user.id)
 
     respond_to do |format|
       format.html
