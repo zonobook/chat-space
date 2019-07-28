884bf266ada7502517eb397086287bf84d32f2ab
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-07-28 12:35:54 +0900

    reloadMessages

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -43,4 +43,21 @@ $(function(){
       alert('error');
     })
   })
-})
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message :last').data('id');
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: location.pathname,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+});
