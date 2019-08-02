bea14d0a1d9ec4ef17e8f6ad4a4c8636bf7596ab
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-08-02 17:08:35 +0900

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ここにバケット名を入れます'
+  config.fog_directory  = 'tsuyoshi-uezono'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tsuyoshi-uezono'
 end
