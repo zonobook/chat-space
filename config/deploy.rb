987e5415e3536fbe6d400cb6c2766cf881e5b668
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2019-08-02 16:51:07 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,14 +14,6 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
-
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
